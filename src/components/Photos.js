51c59b90b776f07d3825fc682f8798dcc1483384
Rob Weber <rob.weber87@gmail.com>
2017-07-16 22:34:46 -0400

    remove duplicate xmas and serve smaller image

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -26,13 +26,7 @@ function Photos(){
 					</Slide>
 					<Slide
 						id="photo-slide"
-						src="/images/first-xmas-12-25-08.jpg"
-						title="Our First Christmas 12-25-08"
-						placement="left">
-					</Slide>
-					<Slide
-						id="photo-slide"
-						src="/images/first-xmas-12-25-08.jpg"
+						src="/images/first-xmas-12-25-08.png"
 						title="Our First Christmas 12-25-08"
 						placement="left">
 					</Slide>
