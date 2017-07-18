0671ed95dfd8c5751ea765e2c3cf8257305d1a19
Rob Weber <rob.weber87@gmail.com>
2017-07-18 14:47:06 -0400

    change navbar to show collapsible only when logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,6 +10,8 @@ function NavBar(props){
             })
     });
 
+    var mobileNavElement = localStorage.token ? <a href="/" data-activates="mobile-demo" className="button-collapse brand-logo headline-text left">Victor and Chad's Wedding<i className="material-icons">menu</i></a> : <a href="/" className="brand-logo headline-text center">Victor and Chad's Wedding</a>
+
 	return(
 		<div className='navbar'>
 			<nav>      
@@ -17,7 +19,7 @@ function NavBar(props){
 	                  <div className='container'>
 	                    <div id='navbar-row' className='row'>
 	                      <a href="/" className="brand-logo headline-text hide-on-med-and-down">Victor and Chad's Wedding</a>
-	                      <a href="/" data-activates="mobile-demo" className="button-collapse brand-logo headline-text left">Victor and Chad's Wedding<i className="material-icons">menu</i></a>
+	                      {mobileNavElement}
 					      <ul id="mobile-demo" className="side-nav">
 					      	<li><Link to="/">Home</Link></li>
 					        <li><Link to="/whereitallbegan">How It All Began</Link></li>
