deece1442da179b9968f0fc037fc1cfe156cf628
Rob Weber <rob.weber87@gmail.com>
2017-07-18 14:31:25 -0400

    add collapsible hamburger to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,7 +17,7 @@ function NavBar(props){
 	                  <div className='container'>
 	                    <div id='navbar-row' className='row'>
 	                      <a href="/" className="brand-logo headline-text hide-on-med-and-down">Victor and Chad's Wedding</a>
-	                      <a href="/" data-activates="mobile-demo" className="button-collapse brand-logo headline-text left">Victor and Chad's Wedding</a>
+	                      <a href="/" data-activates="mobile-demo" className="button-collapse brand-logo headline-text left">Victor and Chad's Wedding<i className="material-icons">menu</i></a>
 					      <ul id="mobile-demo" className="side-nav">
 					      	<li><Link to="/">Home</Link></li>
 					        <li><Link to="/whereitallbegan">How It All Began</Link></li>
